fcd57fdade8b1dc5e4716efb224f19ea04cd6e1a
Kathleen Oneal <onealkathleena@johndeere.com>
2016-05-09 13:04:34 -0500

    cleaned up writer tests --Signed-off-by Kathleen Oneal <onealkathleena@gmail.com>

--- ISOv4PluginLogTest/ExportMappers/BinaryWriterTest.cs
+++ ISOv4PluginLogTest/ExportMappers/BinaryWriterTest.cs
@@ -8,7 +8,6 @@ using AgGateway.ADAPT.ApplicationDataModel.Shapes;
 using AgGateway.ADAPT.ISOv4Plugin.ExportMappers;
 using AgGateway.ADAPT.ISOv4Plugin.ImportMappers;
 using AgGateway.ADAPT.ISOv4Plugin.ImportMappers.LogMappers;
-using AgGateway.ADAPT.ISOv4Plugin.Representation;
 using AgGateway.ADAPT.Representation.RepresentationSystem;
 using AgGateway.ADAPT.Representation.RepresentationSystem.ExtensionMethods;
 using AgGateway.ADAPT.Representation.UnitSystem;
@@ -36,9 +35,12 @@ namespace ISOv4PluginLogTest.ExportMappers
         [SetUp]
         public void Setup()
         {
+            _dataPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(_dataPath);
+            _fileName = "test.bin";
             _meters = new List<Meter>();
-            _spatialRecord = new SpatialRecord{Geometry = new Point { X = 93.6208, Y = 41.5908 }};
-            _spatialRecords = new List<SpatialRecord>{ _spatialRecord };
+            _spatialRecord = new SpatialRecord {Geometry = new Point {X = 93.6208, Y = 41.5908}};
+            _spatialRecords = new List<SpatialRecord> {_spatialRecord};
 
             _numericMeter = new NumericMeter
             {
@@ -51,7 +53,7 @@ namespace ISOv4PluginLogTest.ExportMappers
             {
                 Representation = RepresentationInstanceList.dtSkyCondition.ToModelRepresentation(),
                 SectionId = 1,
-                ValueCodes = new List<int> { 0, 1, 2 }
+                ValueCodes = new List<int> {0, 1, 2}
             };
 
             _numericValueMapperMock = new Mock<INumericValueMapper>();
@@ -62,8 +64,6 @@ namespace ISOv4PluginLogTest.ExportMappers
         [Test]
         public void GivenFilenameWhenWriteThenFileIsCreated()
         {
-            _dataPath = Path.GetTempPath();
-            _fileName = "test.bin";
             var filename = Path.Combine(_dataPath, _fileName);
 
             _binaryWriter.Write(filename, new List<Meter>(), new List<SpatialRecord>());
@@ -80,7 +80,7 @@ namespace ISOv4PluginLogTest.ExportMappers
 
             Write();
 
-            var expectedBytes = new List<byte> { 0x00, 0x74, 0xB7, 0x01, 0x89, 0x1C };
+            var expectedBytes = new List<byte> {0x00, 0x74, 0xB7, 0x01, 0x89, 0x1C};
             VerifyFileContents(expectedBytes, 0);
         }
 
@@ -89,12 +89,21 @@ namespace ISOv4PluginLogTest.ExportMappers
         {
             // North = (41.5908 / .0000001) = 415908000 = 0x18CA40A0
             // East =  (93.6208 / .0000001) = 936208000 = 0x37CD6680
-            _spatialRecord.Geometry = new Point { X = 93.6208, Y = 41.5908 };
+            _spatialRecord.Geometry = new Point {X = 93.6208, Y = 41.5908};
 
             Write();
 
-            var expectedBytes = new List<byte> { 0xA0, 0x40, 0xCA, 0x18, 
-                                                 0x80, 0x66, 0xCD, 0x37 };
+            var expectedBytes = new List<byte>
+            {
+                0xA0,
+                0x40,
+                0xCA,
+                0x18,
+                0x80,
+                0x66,
+                0xCD,
+                0x37
+            };
 
             VerifyFileContents(expectedBytes, 6);
         }
@@ -103,7 +112,7 @@ namespace ISOv4PluginLogTest.ExportMappers
         public void GivenSpatialRowWithNumericMeterWhenWriteThenDlvIsWritten()
         {
             _numericMeter.Id.UniqueIds.Add(GenerateUniqueId(0));
-            _meters.AddRange(new List<Meter> { _numericMeter });
+            _meters.AddRange(new List<Meter> {_numericMeter});
 
             _spatialRecord.SetMeterValue(_numericMeter, new NumericRepresentationValue());
 
@@ -111,7 +120,7 @@ namespace ISOv4PluginLogTest.ExportMappers
 
             Write();
 
-            var expectedBytes = new List<byte> { 0x01, 0x00, 0x14, 0x00, 0x00, 0x00 };
+            var expectedBytes = new List<byte> {0x01, 0x00, 0x14, 0x00, 0x00, 0x00};
 
             VerifyFileContents(expectedBytes, 24);
         }
@@ -120,14 +129,14 @@ namespace ISOv4PluginLogTest.ExportMappers
         public void GivenSpatialRowWithEnumeratedMeterWhenWriteThenDlvIsWritten()
         {
             _enumeratedMeter.Id.UniqueIds.Add(GenerateUniqueId(0));
-            _meters.AddRange(new List<Meter> { _enumeratedMeter });
+            _meters.AddRange(new List<Meter> {_enumeratedMeter});
 
             _spatialRecord.SetMeterValue(_enumeratedMeter, new EnumeratedValue());
             _enumeratedMeterMapperMock.Setup(x => x.Map(_enumeratedMeter, _meters, _spatialRecord)).Returns(1);
 
             Write();
 
-            var expectedBytes = new List<byte> { 0x01, 0x00, 0x01, 0x00, 0x00, 0x00 };
+            var expectedBytes = new List<byte> {0x01, 0x00, 0x01, 0x00, 0x00, 0x00};
             VerifyFileContents(expectedBytes, 24);
         }
 
@@ -136,7 +145,7 @@ namespace ISOv4PluginLogTest.ExportMappers
         {
             _enumeratedMeter.Id.UniqueIds.Add(GenerateUniqueId(0));
             _numericMeter.Id.UniqueIds.Add(GenerateUniqueId(1));
-            _meters.AddRange(new List<Meter> { _enumeratedMeter, _numericMeter });
+            _meters.AddRange(new List<Meter> {_enumeratedMeter, _numericMeter});
 
             _spatialRecord.SetMeterValue(_enumeratedMeter, new EnumeratedValue());
             _spatialRecord.SetMeterValue(_numericMeter, new NumericRepresentationValue());
@@ -148,9 +157,17 @@ namespace ISOv4PluginLogTest.ExportMappers
 
             var expectedBytes = new List<byte>
             {
-                0x02,                           // number of DLVs to follow
-                0x00, 0x01, 0x00, 0x00, 0x00,   // DLV0 and value
-                0x01, 0x14, 0x00, 0x00, 0x00    // DLV1 and value
+                0x02, // number of DLVs to follow
+                0x00,
+                0x01,
+                0x00,
+                0x00,
+                0x00, // DLV0 and value
+                0x01,
+                0x14,
+                0x00,
+                0x00,
+                0x00 // DLV1 and value
             };
 
             VerifyFileContents(expectedBytes, 24);
@@ -161,18 +178,18 @@ namespace ISOv4PluginLogTest.ExportMappers
         {
             _enumeratedMeter.Id.UniqueIds.Add(GenerateUniqueId(0));
             _numericMeter.Id.UniqueIds.Add(GenerateUniqueId(1));
-            _meters.AddRange(new List<Meter> { _enumeratedMeter, _numericMeter });
+            _meters.AddRange(new List<Meter> {_enumeratedMeter, _numericMeter});
 
             var spatialRecord1 = new SpatialRecord
             {
                 Timestamp = new DateTime(2000, 1, 1, 8, 0, 0),
-                Geometry = new Point { X = 93.6208, Y = 41.5908 }
+                Geometry = new Point {X = 93.6208, Y = 41.5908}
             };
 
             spatialRecord1.SetMeterValue(_enumeratedMeter,
                 new EnumeratedValue
                 {
-                    Value = new AgGateway.ADAPT.ApplicationDataModel.Representations.EnumerationMember { Code = 1 }
+                    Value = new AgGateway.ADAPT.ApplicationDataModel.Representations.EnumerationMember {Code = 1}
                 });
             spatialRecord1.SetMeterValue(_numericMeter, new NumericRepresentationValue());
             _numericValueMapperMock.Setup(x => x.Map(_numericMeter, spatialRecord1)).Returns(20);
@@ -180,18 +197,18 @@ namespace ISOv4PluginLogTest.ExportMappers
             var spatialRecord2 = new SpatialRecord
             {
                 Timestamp = new DateTime(2000, 1, 1, 8, 0, 0),
-                Geometry = new Point { X = 93.6208, Y = 41.5908 }
+                Geometry = new Point {X = 93.6208, Y = 41.5908}
             };
 
             spatialRecord2.SetMeterValue(_enumeratedMeter,
                 new EnumeratedValue
                 {
-                    Value = new AgGateway.ADAPT.ApplicationDataModel.Representations.EnumerationMember { Code = 2 }
+                    Value = new AgGateway.ADAPT.ApplicationDataModel.Representations.EnumerationMember {Code = 2}
                 });
             spatialRecord2.SetMeterValue(_numericMeter, new NumericRepresentationValue());
             _numericValueMapperMock.Setup(x => x.Map(_numericMeter, spatialRecord2)).Returns(30);
 
-            _spatialRecords = new List<SpatialRecord> { spatialRecord1, spatialRecord2 };
+            _spatialRecords = new List<SpatialRecord> {spatialRecord1, spatialRecord2};
             _enumeratedMeterMapperMock.Setup(x => x.Map(_enumeratedMeter, _meters, spatialRecord1)).Returns(1);
             _enumeratedMeterMapperMock.Setup(x => x.Map(_enumeratedMeter, _meters, spatialRecord2)).Returns(2);
 
@@ -199,22 +216,76 @@ namespace ISOv4PluginLogTest.ExportMappers
 
             var expectedBytes = new List<byte>
             {
-                0x00, 0x74, 0xB7, 0x01, 0x89, 0x1C,                 // SpatialRecord1 TimeStart
-                0xA0, 0x40, 0xCA, 0x18,                             // SpatialRecord1 Position North
-                0x80, 0x66, 0xCD, 0x37,                             // SpatialRecord1 Position East
-                0x00, 0x00, 0x00, 0x00,                             // SpatialRecord1 Position Up
-                0x00, 0x0B, 0x01, 0x03, 0x89, 0x1C,                 // SpatialRecord1 GpsUtcTime and Date
-                0x02,                                               // number of DLVs to follow
-                0x00, 0x01, 0x00, 0x00, 0x00,                       // DLV0 and value
-                0x01, 0x14, 0x00, 0x00, 0x00,                       // DLV1 and value
-                0x00, 0x74, 0xB7, 0x01, 0x89, 0x1C,                 // SpatialRecord2 TimeStart
-                0xA0, 0x40, 0xCA, 0x18,                             // SpatialRecord1 Position North
-                0x80, 0x66, 0xCD, 0x37,                             // SpatialRecord1 Position East
-                0x00, 0x00, 0x00, 0x00,                             // SpatialRecord1 Position Up
-                0x00, 0x0B, 0x01, 0x03, 0x89, 0x1C,                 // SpatialRecord2 GpsUtcTime and Date
-                0x02,                                               // number of DLVs to follow
-                0x00, 0x02, 0x00, 0x00, 0x00,                       // DLV0 and value
-                0x01, 0x1E, 0x00, 0x00, 0x00                        // DLV1 and value
+                0x00,
+                0x74,
+                0xB7,
+                0x01,
+                0x89,
+                0x1C, // SpatialRecord1 TimeStart
+                0xA0,
+                0x40,
+                0xCA,
+                0x18, // SpatialRecord1 Position North
+                0x80,
+                0x66,
+                0xCD,
+                0x37, // SpatialRecord1 Position East
+                0x00,
+                0x00,
+                0x00,
+                0x00, // SpatialRecord1 Position Up
+                0x00,
+                0x0B,
+                0x01,
+                0x03,
+                0x89,
+                0x1C, // SpatialRecord1 GpsUtcTime and Date
+                0x02, // number of DLVs to follow
+                0x00,
+                0x01,
+                0x00,
+                0x00,
+                0x00, // DLV0 and value
+                0x01,
+                0x14,
+                0x00,
+                0x00,
+                0x00, // DLV1 and value
+                0x00,
+                0x74,
+                0xB7,
+                0x01,
+                0x89,
+                0x1C, // SpatialRecord2 TimeStart
+                0xA0,
+                0x40,
+                0xCA,
+                0x18, // SpatialRecord1 Position North
+                0x80,
+                0x66,
+                0xCD,
+                0x37, // SpatialRecord1 Position East
+                0x00,
+                0x00,
+                0x00,
+                0x00, // SpatialRecord1 Position Up
+                0x00,
+                0x0B,
+                0x01,
+                0x03,
+                0x89,
+                0x1C, // SpatialRecord2 GpsUtcTime and Date
+                0x02, // number of DLVs to follow
+                0x00,
+                0x02,
+                0x00,
+                0x00,
+                0x00, // DLV0 and value
+                0x01,
+                0x1E,
+                0x00,
+                0x00,
+                0x00 // DLV1 and value
             };
 
             VerifyFileContents(expectedBytes, 0);
@@ -224,15 +295,15 @@ namespace ISOv4PluginLogTest.ExportMappers
         public void GivenSpatialRecordWithEnumeratedMetersWithSameIdsWhenWriteThenEnumeratedMeterIsCombined()
         {
             var isoEnumeratedMeter = new ISOEnumeratedMeter();
-            _meters.AddRange(new List<Meter> { isoEnumeratedMeter });
+            _meters.AddRange(new List<Meter> {isoEnumeratedMeter});
 
             var spatialRecord = new SpatialRecord
             {
                 Timestamp = new DateTime(2000, 1, 1, 8, 0, 0),
-                Geometry = new Point { X = 93.6208, Y = 41.5908 }
+                Geometry = new Point {X = 93.6208, Y = 41.5908}
             };
             spatialRecord.SetMeterValue(isoEnumeratedMeter, new EnumeratedValue());
-            _spatialRecords = new List<SpatialRecord> { spatialRecord };
+            _spatialRecords = new List<SpatialRecord> {spatialRecord};
 
             const uint value = 0x12345678;
             _enumeratedMeterMapperMock.Setup(x => x.Map(isoEnumeratedMeter, _meters, spatialRecord)).Returns(value);
@@ -241,13 +312,36 @@ namespace ISOv4PluginLogTest.ExportMappers
 
             var expectedBytes = new List<byte>
             {
-                0x00, 0x74, 0xB7, 0x01, 0x89, 0x1C,                 // SpatialRecord1 TimeStart
-                0xA0, 0x40, 0xCA, 0x18,                             // SpatialRecord1 Position North
-                0x80, 0x66, 0xCD, 0x37,                             // SpatialRecord1 Position East
-                0x00, 0x00, 0x00, 0x00,                             // SpatialRecord1 Position Up
-                0x00, 0x0B, 0x01, 0x03, 0x89, 0x1C,                 // SpatialRecord1 GpsUtcTime and Date
-                0x01,                                               // number of DLVs to follow
-                0x00, 0x78, 0x56, 0x34, 0x12,                       // DLV0 and value
+                0x00,
+                0x74,
+                0xB7,
+                0x01,
+                0x89,
+                0x1C, // SpatialRecord1 TimeStart
+                0xA0,
+                0x40,
+                0xCA,
+                0x18, // SpatialRecord1 Position North
+                0x80,
+                0x66,
+                0xCD,
+                0x37, // SpatialRecord1 Position East
+                0x00,
+                0x00,
+                0x00,
+                0x00, // SpatialRecord1 Position Up
+                0x00,
+                0x0B,
+                0x01,
+                0x03,
+                0x89,
+                0x1C, // SpatialRecord1 GpsUtcTime and Date
+                0x01, // number of DLVs to follow
+                0x00,
+                0x78,
+                0x56,
+                0x34,
+                0x12, // DLV0 and value
             };
 
             VerifyFileContents(expectedBytes, 0);
@@ -281,8 +375,6 @@ namespace ISOv4PluginLogTest.ExportMappers
 
         private void Write()
         {
-            _dataPath = Path.GetTempPath();
-            _fileName = "test.bin";
             var filename = Path.Combine(_dataPath, _fileName);
 
             _binaryWriter.Write(filename, _meters, _spatialRecords);
@@ -291,9 +383,8 @@ namespace ISOv4PluginLogTest.ExportMappers
         [TearDown]
         public void TearDown()
         {
-            if (File.Exists(Path.Combine(_dataPath, _fileName)))
-                File.Delete(Path.Combine(_dataPath, _fileName));
+            if (Directory.Exists(_dataPath))
+                Directory.Delete(_dataPath, true);
         }
-
     }
 }
