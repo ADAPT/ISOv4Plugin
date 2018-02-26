3eac5c32885d522b19f36178d8f87920014b6bc2
Steve Hipwell <steve.hipwell@gmail.com>
2018-02-26 20:45:20 +0000

    Initial build changes

---
+++ package.ps1
@@ -0,0 +1,40 @@
+Param(
+  [Parameter(Mandatory = $true)]
+  [string] $tag,
+  [string] $apiKey
+)
+
+$ErrorActionPreference = "Stop";
+
+$version = $tag -match '(?<=^v)\d+\.\d+\.\d+(?:.+)?$'
+$versionNumber = $version -match '^\d+\.\d+\.\d+'
+if ([string]::IsNullOrEmpty($version))
+{
+    throw "Incorrect tag format"
+}
+
+git fetch -a
+git checkout $tag
+if ($LastExitCode -ne 0) {
+  throw "Could not checkout tag"
+}
+
+nuget restore
+msbuild isoPlugin.sln /m /p:Configuration=Release /p:Version=$version /p:FileVersion=$versionNumber.0 /t:rebuild /v:minimal
+if ($LastExitCode -ne 0) {
+  throw "Solution could no be built"
+}
+
+nuget pack ./AgGatewayISOPlugin.nuspec -verbosity detailed -outputdirectory ./dist -version $version
+if ($LastExitCode -ne 0) {
+  throw "Package could no be built"
+}
+
+if (-not ([string]::IsNullOrEmpty($apiKey))) {
+  nuget push ./dist/AgGatewayISOPlugin.$version.nupkg -apikey $apiKey -source https://api.nuget.org/v3/index.json
+  if ($LastExitCode -ne 0) {
+    throw "Package could no be published"
+  }
+}
+
+# git checkout master
