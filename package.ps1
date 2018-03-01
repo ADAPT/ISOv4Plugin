59955a17d9215d2d303fabf87942a674c6d436e4
Steve Hipwell <steve.hipwell@gmail.com>
2018-03-01 14:27:10 +0000

    Update build script for local testing

--- package.ps1
+++ package.ps1
@@ -1,12 +1,13 @@
 Param(
   [Parameter(Mandatory = $true)]
-  [string] $tag,
-  [string] $apiKey
+  [string] $Tag,
+  [bool] $UseBranch,
+  [string] $ApiKey
 )
 
 $ErrorActionPreference = "Stop";
 
-if ($tag -match '(?<=^v)\d+\.\d+\.\d+(?:.+)?$') {
+if ($Tag -match '(?<=^v)\d+\.\d+\.\d+(?:.+)?$') {
   $version = $Matches[0]
 }
 else {
@@ -20,20 +21,25 @@ else {
   throw "Incorrect tag format"
 }
 
-$branch = git symbolic-ref --short HEAD
+if (-not $UseBranch) {
+  $branch = git symbolic-ref --short HEAD
 
-git fetch -a
-git checkout $tag
-if ($LastExitCode -ne 0) {
-  throw "Could not checkout tag"
+  git fetch -a
+  git checkout $Tag
+  if ($LastExitCode -ne 0) {
+    throw "Could not checkout tag"
+  }
 }
+
+Remove-Item -Path ./ISOv4Plugin/bin -Recurse -Force
+Remove-Item -Path ./ISOv4Plugin/obj -Recurse -Force
 
 nuget restore
 if ($LastExitCode -ne 0) {
   throw "NuGet packages could not be restored"
 }
 
-msbuild IsoPlugin.sln /m /p:Configuration=Release /p:Version=$version /p:FileVersion=$versionNumber.0 /t:rebuild /v:minimal
+msbuild ./ISOv4Plugin/ISOv4Plugin.csproj /m /p:Configuration=Release /p:Version=$version /p:FileVersion=$versionNumber.0 /t:rebuild /v:minimal
 if ($LastExitCode -ne 0) {
   throw "Solution could no be built"
 }
@@ -43,11 +49,13 @@ if ($LastExitCode -ne 0) {
   throw "Package could no be built"
 }
 
-if (-not ([string]::IsNullOrEmpty($apiKey))) {
-  nuget push ./dist/AgGatewayISOPlugin.$version.nupkg -apikey $apiKey -source https://api.nuget.org/v3/index.json
+if (-not ([string]::IsNullOrEmpty($ApiKey))) {
+  nuget push ./dist/AgGatewayISOPlugin.$version.nupkg -apikey $ApiKey -source https://api.nuget.org/v3/index.json
   if ($LastExitCode -ne 0) {
     throw "Package could no be published"
   }
 }
 
-git checkout $branch
+if (-not $UseBranch) {
+  git checkout $branch
+}
