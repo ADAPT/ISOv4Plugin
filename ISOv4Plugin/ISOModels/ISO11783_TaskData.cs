46863fb3dbf1640f09f7a752d0785c451aec140c
Martin Sperlich <martin.sperlich@claas.com>
2019-10-24 14:04:24 +0200

    Set ISO11783_TaskData attributes using properties.

--- ISOv4Plugin/ISOModels/ISO11783_TaskData.cs
+++ ISOv4Plugin/ISOModels/ISO11783_TaskData.cs
@@ -18,9 +18,12 @@ namespace AgGateway.ADAPT.ISOv4Plugin.ISOModels
     {
         public ISO11783_TaskData()
         {
+            XmlComments = new List<string>();  //191022 MSp
+
             ChildElements = new List<ISOElement>();
         }
 
+        public List<string> XmlComments { get; set; }  //191022 MSp
         public List<ISOElement> ChildElements { get; set; }
         public ISO11783_LinkList LinkList { get; set; }
         public string DataFolder { get; set; }
@@ -50,6 +53,8 @@ namespace AgGateway.ADAPT.ISOv4Plugin.ISOModels
             {
                 xmlBuilder.WriteAttributeString("DataTransferLanguage", DataTransferLanguage);
             }
+
+            foreach (string item in XmlComments) { xmlBuilder.WriteComment(item); }    //191022 MSp
 
             if (ChildElements != null)
             {
